b2cf7468a83141f8fee928457f06f38200dc4bcc
arnauddlbghe <arnauddelberghe@gmail.com>
2025-03-27 23:19:08 +0100

    First commit. Initialize project

---
+++ src/main/java/com/delberghea/structural/bridge/Main.java
@@ -0,0 +1,7 @@
+package com.delberghea.structural.bridge;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/delberghea/structural/proxy/Main.java
@@ -0,0 +1,8 @@
+package com.delberghea.structural.proxy;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+}
