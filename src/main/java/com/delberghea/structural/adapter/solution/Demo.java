925308a5a7d534505da9f15a1c25624d375164b3
Arnaud <59831564+arnauddlbghe@users.noreply.github.com>
2025-03-29 18:36:57 +0100

    Adapter (#4)
    * adapter: first implementation
    
    * adapter: update implementation

--- src/main/java/com/delberghea/structural/adapter/solution/Demo.java
+++ src/main/java/com/delberghea/structural/adapter/solution/Demo.java
@@ -1,9 +1,24 @@
-package com.delberghea.structural.adapter;
+package com.delberghea.structural.adapter.solution;
 
-public class Main {
+import com.delberghea.structural.adapter.solution.model.RoundHole;
+import com.delberghea.structural.adapter.solution.model.RoundPeg;
+import com.delberghea.structural.adapter.solution.model.SquarePeg;
+import com.delberghea.structural.adapter.solution.model.SquarePegAdapter;
+
+public class Demo {
     public static void main(String[] args) {
-        //
+        // Let's first check that our roundHole and our roundPeg fit perfectly.
+        RoundHole roundHole = new RoundHole(10);
+        RoundPeg roundPeg = new RoundPeg(10);
+        System.out.println(roundHole.fits(roundPeg));
 
-        System.out.println("Hello world!");
+        // Will not compile. Can't check directly if squarePeg fit in roundHole.
+        // SquarePeg squarePeg = new SquarePeg(5);
+        // System.out.println(roundHole.fits(squarePeg));
+
+        // But now, we can check if our square will fit in our roundHole.
+        SquarePeg squarePeg = new SquarePeg(5);
+        SquarePegAdapter squarePegAdapter = new SquarePegAdapter(squarePeg);
+        System.out.println(roundHole.fits(squarePegAdapter));
     }
 }
--- src/main/java/com/delberghea/structural/adapter/solution/model/SquarePeg.java
+++ src/main/java/com/delberghea/structural/adapter/solution/model/SquarePeg.java
@@ -1,4 +1,19 @@
 package com.delberghea.structural.adapter.solution.model;
 
 public class SquarePeg {
+
+    private int width;
+
+    public SquarePeg(int width) {
+        this.width = width;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
 }
